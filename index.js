3423e1242770b0d4ad2aa03c45c92c661d73e118
Rami Platero <ramiplatero11@gmail.com>
2023-10-01 17:11:11 -0300

    add function to update cart total

--- index.js
+++ index.js
@@ -24,20 +24,28 @@ const products = [
   },
 ];
 
-  // Funcion que dinamicamente muestra distintos prompts en caso de que tengamos un total a pagar
-  const optionsPrompt = (total) => {
-    if (total > 0) {
-      return prompt(
-        `¡Bienvenido a Gaming Components!\nNos complace ofrecerte una experiencia excepcional de compra de componentes para juegos. En nuestro catálogo, encontrarás una amplia selección de las últimas y más avanzadas tarjetas gráficas de alta gama.\nDESCUENTO: Si el monto total de tu compra supera los $10000 USD se aplicará un descuento del 20%!\n\nPara continuar selecciona una opción ingresando el número correspondiente en el menu de opciones.\nMonto total: $${total.toLocaleString(
-          "en-US"
-        )}.00\n\n1. Compra de productos\n2. Mi carrito\n3. Pagar\nEscribe SALIR para salir.`
-      );
-    } else {
-      return prompt(
-        `¡Bienvenido a Gaming Components!\nNos complace ofrecerte una experiencia excepcional de compra de componentes para juegos. En nuestro catálogo, encontrarás una amplia selección de las últimas y más avanzadas tarjetas gráficas de alta gama.\nDESCUENTO: Si el monto total de tu compra supera los $10000 USD se aplicará un descuento del 20%!\n\nPara continuar selecciona una opción ingresando el número correspondiente en el menu de opciones.\n\n1. Compra de productos\n2. Mi carrito\n3. Pagar\nEscribe SALIR para salir.`
-      );
-    }
-  };
+// Funcion que dinamicamente muestra distintos prompts en caso de que tengamos un total a pagar
+const optionsPrompt = (total) => {
+  if (total > 0) {
+    return prompt(
+      `¡Bienvenido a Gaming Components!\nNos complace ofrecerte una experiencia excepcional de compra de componentes para juegos. En nuestro catálogo, encontrarás una amplia selección de las últimas y más avanzadas tarjetas gráficas de alta gama.\nDESCUENTO: Si el monto total de tu compra supera los $10000 USD se aplicará un descuento del 20%!\n\nPara continuar selecciona una opción ingresando el número correspondiente en el menu de opciones.\nMonto total: $${total.toLocaleString(
+        "en-US"
+      )}.00\n\n1. Compra de productos\n2. Mi carrito\n3. Pagar\nEscribe SALIR para salir.`
+    );
+  } else {
+    return prompt(
+      `¡Bienvenido a Gaming Components!\nNos complace ofrecerte una experiencia excepcional de compra de componentes para juegos. En nuestro catálogo, encontrarás una amplia selección de las últimas y más avanzadas tarjetas gráficas de alta gama.\nDESCUENTO: Si el monto total de tu compra supera los $10000 USD se aplicará un descuento del 20%!\n\nPara continuar selecciona una opción ingresando el número correspondiente en el menu de opciones.\n\n1. Compra de productos\n2. Mi carrito\n3. Pagar\nEscribe SALIR para salir.`
+    );
+  }
+};
+
+// Actualiza el total del carrito en base a los productos agregados
+const updateCartTotal = () => {
+  const total = cart.products.reduce((newTotal, cartProduct) => {
+    return (newTotal += cartProduct.total);
+  }, 0);
+  return (cart.total = total);
+};
 
 const main = () => {
   let option;
@@ -46,10 +54,10 @@ const main = () => {
   while (option.trim().toUpperCase() !== "SALIR") {
     switch (parseInt(option)) {
       case 1:
-        alert("Compra de productos")
+        alert("Compra de productos");
         break;
       case 2:
-        alert("Carrito de compras")
+        alert("Carrito de compras");
         break;
       case 3:
         alert("Pagando...");

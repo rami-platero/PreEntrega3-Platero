91dafceecd5a554704eefea9b637ed00463e686b
Rami Platero <ramiplatero11@gmail.com>
2023-10-01 14:00:12 -0300

    add array of products

--- index.js
+++ index.js
@@ -5,6 +5,28 @@ const product3 = "Gigabyte GeForce RTX 4080 Gaming";
 const product4 = "MSI GeForce RTX 3090 Ti";
 const product5 = "ZOTAC Gaming GeForce RTX 3080 Ti";
 
+const products = [
+  {
+    name: "ASUS ROG Strix NVIDIA GeForce RTX 4070 Ti",
+    price: 930,
+  },
+  {
+    name: "MSI Gaming GeForce RTX 4060 Ti",
+    price: 400,
+  },
+  {
+    name: "Gigabyte GeForce RTX 4080 Gaming",
+    price: 1200,
+  },
+  {
+    name: "MSI GeForce RTX 3090 Ti",
+    price: 1800,
+  },
+  {
+    name: "ZOTAC Gaming GeForce RTX 3080 Ti",
+    price: 1250,
+  },
+];
 // calcula el precio final basado en la cantidad de cierto producto
 const calculateCost = (price, product) => {
   let amount;
@@ -28,38 +50,40 @@ const calculateCost = (price, product) => {
   return price * amount;
 };
 
-const main = (total=0) => {
-  let option 
+const main = (total = 0) => {
+  let option;
 
   // si el total es diferente a 0 es porque ya hemos comprado antes.
-  if(total===0){
+  if (total === 0) {
     option = prompt(
-        "Bienvenido a Gaming Components.\nAqui podras comprar placas de videos de ultima gama, si deseas comprar alguna, elige alguna de las opciones de la siguiente lista para continuar.\n(si el monto total supera los $10000 USD se aplicará un descuento del 20%)\n\n1. " +
-          product1 +
-          " - $930.00\n2. " +
-          product2 +
-          " - $400.00\n3. " +
-          product3 +
-          " - $1200.00\n4. " +
-          product4 +
-          " - $1800.00\n5. " +
-          product5 +
-          " - $1250.00\n\nEscribe SALIR para salir."
-      );
+      "Bienvenido a Gaming Components.\nAqui podras comprar placas de videos de ultima gama, si deseas comprar alguna, elige alguna de las opciones de la siguiente lista para continuar.\n(si el monto total supera los $10000 USD se aplicará un descuento del 20%)\n\n1. " +
+        product1 +
+        " - $930.00\n2. " +
+        product2 +
+        " - $400.00\n3. " +
+        product3 +
+        " - $1200.00\n4. " +
+        product4 +
+        " - $1800.00\n5. " +
+        product5 +
+        " - $1250.00\n\nEscribe SALIR para salir."
+    );
   } else {
     option = prompt(
-        "Bienvenido de vuelta.\nElige uno de los siguientes productos a comprar de la siguiente lista\n\nMonto total: $"+total.toLocaleString()+" USD\n\n1. " +
-          product1 +
-          " - $930.00\n2. " +
-          product2 +
-          " - $400.00\n3. " +
-          product3 +
-          " - $1200.00\n4. " +
-          product4 +
-          " - $1800.00\n5. " +
-          product5 +
-          " - $1250.00\n\nEscribe SALIR para salir."
-      );
+      "Bienvenido de vuelta.\nElige uno de los siguientes productos a comprar de la siguiente lista\n\nMonto total: $" +
+        total.toLocaleString() +
+        " USD\n\n1. " +
+        product1 +
+        " - $930.00\n2. " +
+        product2 +
+        " - $400.00\n3. " +
+        product3 +
+        " - $1200.00\n4. " +
+        product4 +
+        " - $1800.00\n5. " +
+        product5 +
+        " - $1250.00\n\nEscribe SALIR para salir."
+    );
   }
 
   while (option.trim().toUpperCase() !== "SALIR") {
@@ -122,37 +146,38 @@ const main = (total=0) => {
   if (total >= 10000) {
     let final = total - (20 * total) / 100;
     return alert(
-      "Tu monto supera los $10000 por lo tanto es aplicable al descuento!\n\nEl monto final quedaría en $"
-      +final.toLocaleString() + " USD"
+      "Tu monto supera los $10000 por lo tanto es aplicable al descuento!\n\nEl monto final quedaría en $" +
+        final.toLocaleString() +
+        " USD"
     );
   } else {
     let newOption = prompt(
+      "Tu monto no supera los $10000 USD, por lo tanto no se le aplicará el descuento.\n\nMonto final: $" +
+        total.toLocaleString() +
+        " USD" +
+        "\n\nCon $" +
+        (10000 - total).toLocaleString() +
+        " USD mas puedes obtener el 20% de descuento, si quieres continuar comprando ingresa 1, si quieres salir ingresa SALIR"
+    );
+    while (
+      newOption.trim().toUpperCase() !== "SALIR" &&
+      parseInt(newOption) !== 1
+    ) {
+      alert("Opcion no valida.");
+      newOption = prompt(
         "Tu monto no supera los $10000 USD, por lo tanto no se le aplicará el descuento.\n\nMonto final: $" +
           total.toLocaleString() +
           " USD" +
-          "\n\nCon $"
-        +(10000 - total).toLocaleString() +
+          "\n\nCon $" +
+          +(10000 - total).toLocaleString() +
           " USD mas puedes obtener el 20% de descuento, si quieres continuar comprando ingresa 1, si quieres salir ingresa SALIR"
       );
-      while (
-        newOption.trim().toUpperCase() !== "SALIR" &&
-        parseInt(newOption) !== 1
-      ) {
-        alert("Opcion no valida.");
-        newOption = prompt(
-          "Tu monto no supera los $10000 USD, por lo tanto no se le aplicará el descuento.\n\nMonto final: $" +
-            total.toLocaleString() +
-            " USD" +
-            "\n\nCon $"+
-          +(10000 - total).toLocaleString() +
-            " USD mas puedes obtener el 20% de descuento, si quieres continuar comprando ingresa 1, si quieres salir ingresa SALIR"
-        );
-      }
-      if (parseInt(newOption) === 1) {
-        return main(total);
-      } else {
-        return;
-      }
+    }
+    if (parseInt(newOption) === 1) {
+      return main(total);
+    } else {
+      return;
+    }
   }
 };
 

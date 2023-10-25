da7acdea5cbc06d22facce4ab822bacb4d4a73b3
Rami Platero <ramiplatero11@gmail.com>
2023-10-25 13:38:27 -0300

    extend maxWidth: add thumbnail property

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        'thumbnail': '128px',
+      }
+    }
   },
   plugins: [],
 }
